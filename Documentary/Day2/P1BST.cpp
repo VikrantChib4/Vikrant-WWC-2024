4f1eae1f05b4424396bd8bb5d9f89c2699f1149a
VikrantChib4 <22BCS50099@cuchd.in>
2024-12-20 08:53:52 +0000

    h

---
+++ Documentary/Day2/P1BST.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+TreeNode* buildBST(int* nums, int start, int end) {
+    if (start > end) {
+        return nullptr;
+    }
+    
+    int mid = start + (end - start) / 2;
+    TreeNode* root = new TreeNode(nums[mid]);
+    root->left = buildBST(nums, start, mid - 1);
+    root->right = buildBST(nums, mid + 1, end);
+    return root;
+}
+TreeNode* sortedArrayToBST(int* nums, int size) {
+    return buildBST(nums, 0, size - 1);
+}
+void printTree(TreeNode* root) {
+    if (root == nullptr) {
+        return;
+    }
+    printTree(root->left);
+    cout << root->val << " ";
+    printTree(root->right);
+}
+int main() {
+    int nums[] = {-10, -3, 0, 5, 9};
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    TreeNode* root = sortedArrayToBST(nums, size);
+    cout << "In-order traversal of the height-balanced BST: ";
+    printTree(root);
+    cout << endl;
+
+    return 0;
+}
---
+++ Documentary/Day2/P2Sudoku.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+bool isValidSudoku(char board[9][9]) {
+    unordered_set<string> seen;
+
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            char num = board[i][j];
+            if (num != '.') {
+                string row = string(1, num) + " in row " + to_string(i);
+                string col = string(1, num) + " in col " + to_string(j);
+                string box = string(1, num) + " in box " + to_string(i / 3) + "-" + to_string(j / 3);
+                if (seen.count(row) || seen.count(col) || seen.count(box)) {
+                    return false;
+                }
+                seen.insert(row);
+                seen.insert(col);
+                seen.insert(box);
+            }
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    char board[9][9] = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+    };
+
+    if (isValidSudoku(board)) {
+        cout << "The Sudoku board is valid." << endl;
+    } else {
+        cout << "The Sudoku board is invalid." << endl;
+    }
+
+    return 0;
+}
